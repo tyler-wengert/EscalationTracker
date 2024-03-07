6670d91a580d062e1e0b8f8379dd1b3738fca12a
Tyler Wengert <me@tylerwengert.com>
2024-03-07 00:06:56 -0700

    buttons go to tabs

--- Form1.cs
+++ Form1.cs
@@ -16,11 +16,23 @@ namespace EscalationTracker
         public Form1()
         {
             InitializeComponent();
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+            tabControl1.ItemSize = new Size((tabControl1.Width / tabControl1.TabPages.Count) - 1, tabControl1.ItemSize.Height);
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            tabControl1.ItemSize = new Size((tabControl1.Width / tabControl1.TabPages.Count) - 1, tabControl1.ItemSize.Height);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            tabControl1.SelectTab(tabPage2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(tabPage3);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -28,6 +40,14 @@ namespace EscalationTracker
 
         }
 
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
